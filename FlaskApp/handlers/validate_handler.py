99fe5185707215c81ee1ccceeffc4328cfab1829
Michael Odumosu <michaelodumosu57@gmail.com>
2023-10-22 16:30:09 -0400

    [UPDATE] add

--- FlaskApp/handlers/validate_handler.py
+++ FlaskApp/handlers/validate_handler.py
@@ -2,3 +2,9 @@
 
 def post_endpoint(req_data):
   return req_data
+
+
+def receive_doc(data):
+  file_data = data["resume"]["content"].encode()
+  binary_data = base64.b64decode(file_data)
+  my_buffer = BytesIO(binary_data)
