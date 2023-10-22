d3b6dcbc56c5ffc1177e43419403bc3ce60df855
Jae Hahn <hahnandy0201@gmail.com>
2023-10-22 19:02:42 -0400

    last min changes

--- frontend/app/components/dropimage.jsx
+++ frontend/app/components/dropimage.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 export default function PrivatePage(props) {
   const [image, setImage] = useState(null);
   const [createObjectURL, setCreateObjectURL] = useState(null);
+  const [serverResponse, setServerResponse] = useState(null);  // New state variable
 
   const uploadToClient = (event) => {
     if (event.target.files && event.target.files[0]) {
@@ -16,19 +17,27 @@ export default function PrivatePage(props) {
   const uploadToServer = async (event) => {
     const body = new FormData();
     body.append("file", image);
-    const response = await fetch("http://localhost:3001/api/file", {
-      method: "POST",
-      body
-    });
+    // const route = "https://ai4pgprod.onrender.com/validate/receive_rap_sheet";
+    const route = "http://localhost:3000/validate/receive_rap_sheet";
 
+    try {
+      const response = await fetch(route, {
+        method: "POST",
+        body,
+      });
 
-    if (response.ok) {
-      alert("File uploaded successfully!");
-    } else {
-      alert("File upload failed. " + await response.text());
+      if (response.ok) {
+        const responseData = await response.json();
+        setServerResponse(JSON.stringify(responseData, null, 2)); // Convert JSON object to formatted string
+        alert("File uploaded successfully!");
+      } else {
+        alert("File upload failed. " + await response.text());
+      }
+    } catch (error) {
+      console.error("There was an error uploading the file:", error);
+      alert("There was an error uploading the file. Please check the console for more details.");
     }
   };
-
 
   return (
     <div className="flex flex-col items-center space-y-6">
@@ -53,8 +62,10 @@ export default function PrivatePage(props) {
         <button className="btn btn-primary" onClick={uploadToServer}>
           Send to server
         </button>
+
+        {/* Displaying the server response */}
+        {serverResponse && <pre className="mt-4 text-center">{serverResponse}</pre>}
       </div>
     </div>
   );
-
 }
